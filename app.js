0ffdaad9b311e07aead71795e9c62a9d4dbaf4db
stefancure <akpanowoprecious@gmail.com>
2021-10-13 14:09:11 +0100

    Full update

---
+++ app.js
@@ -0,0 +1,121 @@
+import sublinks from './data.js';
+
+const toggleBtn = document.querySelector('.toggle-btn');
+const closeBtn = document.querySelector('.close-btn');
+const sidebarWrapper = document.querySelector('.sidebar-wrapper');
+const sidebar = document.querySelector('.sidebar-links');
+const linkBtns = [...document.querySelectorAll('.link-btn')];
+const gasBtns = [...document.querySelectorAll('.m_btn')];
+const labelDiv = [...document.querySelectorAll('.m_label')];
+const submenu = document.querySelector('.submenu');
+const hero = document.querySelector('.hero');
+const nav = document.querySelector('.my-nav'); 
+const labelPrice = document.querySelector('.label_price');
+const labelPrice1 = document.querySelector('.label_price1');
+const labelPrice2 = document.querySelector('.label_price2');
+const btnHigh = document.querySelector('.high');
+const btnMedium = document.querySelector('.medium');
+const btnLow = document.querySelector('.low');
+// hide/show sideabar
+toggleBtn.addEventListener('click', () => {
+  sidebarWrapper.classList.add('show');
+});
+closeBtn.addEventListener('click', () => {
+  sidebarWrapper.classList.remove('show');
+});
+
+// set sidebar
+sidebar.innerHTML = sublinks
+  .map((item) => {
+    const { links, page } = item;
+    return `<article >
+<h4>${page}</h4>
+<div class="sidebar-sublinks">
+${links
+  .map((link) => {
+    return `<a href="${link.url}"><i class="${link.icon}"></i>${link.label}</a>`;
+  })
+  .join('')}
+</div>
+</article>`;
+  })
+  .join('');
+
+linkBtns.forEach((btn) => {
+  btn.addEventListener('mouseover', function (e) {
+    const text = e.currentTarget.textContent;
+    const tempBtn = e.currentTarget.getBoundingClientRect();
+    const center = (tempBtn.left + tempBtn.right) / 2;
+    const bottom = tempBtn.bottom - 3;
+
+    const tempPage = sublinks.find((link) => link.page === text);
+    if (tempPage) {
+      const { page, links } = tempPage;
+      submenu.classList.add('show');
+      submenu.style.left = `${center}px`;
+      submenu.style.top = `${bottom}px`;
+      // OPTIONAL
+      let columns = 'col-2';
+      if (links.length === 3) {
+        columns = 'col-3';
+      }
+      if (links.length > 3) {
+        columns = 'col-4';
+      }
+      submenu.innerHTML = `
+      <section> 
+      <h4>${page}</h4>
+      <div class="submenu-center ${columns}">
+      ${links
+        .map((link) => {
+          return `<a href="${link.url}" class="link_pop"><i class="${link.icon}"></i>${link.label}</a>`;
+        })
+        .join('')}
+      </div>
+      </section>
+      `;
+    }
+  });
+});
+
+hero.addEventListener('mouseover', function (e) {
+  submenu.classList.remove('show');
+});
+
+gasBtns.forEach((btn) => {
+  btn.addEventListener('click', function (e) {
+    //console.log(e);
+    e.preventDefault();
+    gasBtns.forEach(function(item){
+      if(item !== btn) {
+        item.classList.remove('my_active');
+      }
+    })
+    e.currentTarget.classList.toggle('my_active')
+  })
+});
+
+
+ 
+
+btnHigh.addEventListener('click', function (e) {
+  
+  labelDiv.forEach(function(div){
+    div.classList.add('my_hide');
+  })
+  labelPrice.classList.remove('my_hide');
+});
+btnMedium.addEventListener('click', function (e) {
+  
+  labelDiv.forEach(function(div){
+    div.classList.add('my_hide');
+  })
+  labelPrice1.classList.remove('my_hide');
+});
+btnLow.addEventListener('click', function (e) {
+  
+  labelDiv.forEach(function(div){
+    div.classList.add('my_hide');
+  })
+  labelPrice2.classList.remove('my_hide');
+});
