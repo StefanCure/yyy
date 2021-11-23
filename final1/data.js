e40d7ce4a3b062b4b10cccb998e44429baa632b6
stefancure <akpanowoprecious@gmail.com>
2021-11-23 17:24:25 +0100

    Both file uploaded

---
+++ final1/data.js
@@ -0,0 +1,37 @@
+const sublinks = [
+  {
+    page: 'Home',
+    links: [
+      { label: 'home', url: 'index.html' }
+    ],
+  },
+  {
+    page: 'Borrower Profile',
+    links: [
+      { label: 'collateral', icon: 'fas fa-book', url: 'products.html' },
+      { label: 'contract', icon: 'fas fa-book', url: 'products.html' },
+    ],
+  },
+  {
+    page: 'Lender Profile',
+    links: [
+      { label: 'offers sent', icon: 'fas fa-briefcase', url: 'products.html' },
+      { label: 'contract', icon: 'fas fa-briefcase', url: 'products.html' },
+    ],
+  },
+  {
+    page: 'FAQ',
+    links: [
+      { label: 'FAQ', icon: 'fas fa-briefcase', url: 'products.html' }
+    ],
+  },
+  {
+    page: 'Details',
+    links: [
+      { label: 'Connect', icon: 'fas fa-briefcase', url: 'products.html' },
+      { label: 'Login', icon: 'fas fa-briefcase', url: 'products.html' },
+    ],
+  },
+];
+
+export default sublinks;
